f6dc075ecadedeec7c57fdf611b2c81d69bd1313
Clinio Marcelino <cliniof@gmail.com>
2025-06-24 20:44:43 -0300

    update app.properties

--- src/main/java/com/Raspr0_/Application/dao/DeviceDao.java
+++ src/main/java/com/Raspr0_/Application/dao/DeviceDao.java
@@ -2,6 +2,8 @@ package com.Raspr0_.Application.dao;
 
 import com.Raspr0_.Application.model.DeviceModel;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface DeviceDao extends JpaRepository<DeviceModel, Long> {
 }
